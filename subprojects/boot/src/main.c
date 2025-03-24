43be6b6b964817ff10bb72d11a4adb5f17aa7634
Henrik Karlsson <hakarlsson@proton.me>
2025-03-24 19:17:54 +0100

    Cheshire support.

--- subprojects/boot/src/main.c
+++ subprojects/boot/src/main.c
@@ -1,14 +1,9 @@
 #include "s3k.h"
 #include "serio.h"
-#include "serio/ns16550a.h"
 
 #define N 4
 #define M 3
 
-int serio_putchar(int c)
-{
-	return serio_ns16550a_putchar(c, (void*)0x10000000);
-}
 
 enum {
 	BOOT_PMP = 0,
@@ -38,7 +33,7 @@ void error(int err)
 
 void setup_process(int pid, uint64_t addr)
 {
-	s3k_cap_t uart = s3k_mk_pmp(s3k_napot_encode(0x10000000, 0x20), 0x3);
+	s3k_cap_t uart = s3k_mk_pmp(s3k_napot_encode(0x03002000, 0x20), 0x3);
 	s3k_cap_t ram = s3k_mk_pmp(s3k_napot_encode(addr, 0x10000), 0x7);
 	// Derive RAM
 	error(s3k_cap_derive(RAM_MEM, 10, ram));
@@ -68,7 +63,7 @@ void setup_process(int pid, uint64_t addr)
 void setup_uart(void)
 {
 	s3k_cap_t pmp_uart
-	    = s3k_mk_pmp(s3k_napot_encode(0x10000000, 0x20), 0x3);
+	    = s3k_mk_pmp(s3k_napot_encode(0x03002000, 0x20), 0x3);
 	s3k_cap_derive(2, 8, pmp_uart);
 	s3k_pmp_load(8, 1);
 	s3k_sync();
@@ -76,8 +71,8 @@ void setup_uart(void)
 
 void setup_time(void)
 {
-	s3k_cap_t boot_time0 = s3k_mk_time(0, 0, 1);
-	s3k_cap_t system_time = s3k_mk_time(0, 1, NSLOT);
+	s3k_cap_t boot_time0 = s3k_mk_time(0, 0, 2);
+	s3k_cap_t system_time = s3k_mk_time(0, 2, NSLOT);
 	error(s3k_cap_derive(HART0_TIME, 16, boot_time0));
 	error(s3k_cap_derive(HART0_TIME, 10, system_time));
 	s3k_sleep(0);
@@ -118,19 +113,18 @@ void scheduler(void)
 {
 	int start, end;
 	int system_time_cap = 10;
-	s3k_sleep(0);
 
 	int v[N] = {0, 1, 0, 1};
 	int s[M] = {0, 0, 0};
+	s3k_sleep(0);
 
 	while (1) {
-		s3k_sleep(0);
 		uint64_t start_time = s3k_get_time();
 		error(s3k_cap_revoke(system_time_cap));
 		for (int i = 0; i < N; ++i)
 			v[i] = xorshift() % 2;
 		schedule(v, s);
-		start = 1;
+		start = 2;
 		end = start + s[0];
 		s3k_cap_derive(system_time_cap, 26, s3k_mk_time(0, start, end));
 		start = end;
@@ -151,8 +145,8 @@ void scheduler(void)
 		s3k_mon_resume(MONITOR, PID_FCS);
 		s3k_mon_resume(MONITOR, PID_NAV);
 		uint64_t end_time = s3k_get_time();
-		serio_printf("Slots remaining: %d\n", NSLOT - end);
-		serio_printf("scheduling cost: %D\n", end_time - start_time);
+		serio_printf("boot: %D\n", end_time - start_time);
+		s3k_sleep(0);
 	}
 }
 
--- subprojects/fcs/src/main.c
+++ subprojects/fcs/src/main.c
@@ -1,11 +1,5 @@
 #include "s3k.h"
 #include "serio.h"
-#include "serio/ns16550a.h"
-
-int serio_putchar(int c)
-{
-	return serio_ns16550a_putchar(c, (void*)0x10000000);
-}
 
 int main(void)
 {
